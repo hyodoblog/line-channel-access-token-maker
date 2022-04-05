5a68545192b9a907eb95f597d9e5604a8b04c70d
hyodoblog <yusei.hyodo@youlib.co.jp>
2022-04-05 20:09:56 +0900

    フッターの調整

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,20 @@
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import { SITE_TITLE } from '~/config'
 import styles from '~/styles/Home.module.css'
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <a href="https://twitter.com/hyodoblog" target="_blank" rel="noopener noreferrer">
-        <Typography variant="body1">Powered by</Typography>
-        <Typography variant="body1" sx={{ ml: 1, textDecorationLine: 'underline' }}>
-          hyodoblog
-        </Typography>
-      </a>
+      <Box sx={{ textAlign: 'center' }}>
+        <a href="https://twitter.com/hyodoblog" target="_blank" rel="noopener noreferrer">
+          <Typography variant="caption">Powered by</Typography>
+          <Typography variant="caption" sx={{ ml: 1, textDecorationLine: 'underline' }}>
+            hyodoblog
+          </Typography>
+        </a>
+
+        <Typography variant="caption">Copyright © 2022 {SITE_TITLE}.</Typography>
+      </Box>
     </footer>
   )
 }
